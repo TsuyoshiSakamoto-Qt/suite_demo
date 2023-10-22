05464691be046816dfbb9d0d811cf13cf9752bb5
tssakamo <tsuyoshi.sakamoto@qt.io>
2023-10-23 08:38:35 +0900

    update

--- tst_case2/test.py
+++ tst_case2/test.py
@@ -8,3 +8,4 @@ def main():
     mouseClick(waitForObject(names.hello_World_push_me_Button), Qt.LeftButton)
     mouseClick(waitForObject(names.hello_World_screen01_ui), 214, 232, Qt.LeftButton)
     test.vp("VP1")
+    
